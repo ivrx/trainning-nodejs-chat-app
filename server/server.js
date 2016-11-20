3e4abf3b0a864b548a67aa7aae73cb8f4d78b89f
ivrx <ivrxea@gmail.com>
2016-11-20 10:17:51 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -12,12 +12,34 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected!');
 
+  socket.emit('newMessage', {
+    from: 'admin',
+    text: 'welcome to chat app!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'admin',
+    text: 'new user joined',
+    createdAt: new Date().getTime()
+  });
+
+  // socket.emit from admin, text: welcome to chat app
+  // socket.broadcast.emit from admin, text: new user joined
+
   socket.on('createMessage', (msg) => {
     io.emit('newMessage', {
       from: msg.from,
       text: msg.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: msg.from,
+    //   text: msg.text,
+    //   createdAt: new Date().getTime()
+    // });
+
   });
 
   socket.on('disconnect', () => {
